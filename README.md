23bde86bb7e514556e7a585366a9e07d79aa1377
Ryxn <purkaitryan@gmail.com>
2018-07-05 16:22:34 -0700

    Monitor v1

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-CybersoleMonitor
+# Project Title
+
+Cybersole Monitor
+
+## Getting Started
+
+A) Clone/Download this Repo
+B) Run 'npm install'
+C) Edit config.json to your liking
+D) Create your proxy file and either leave it empty for proxyless or add proxies
+E) Run 'node monitor'
+
+### Prerequisites
+
+You need to have Node.js installed
+
+## Built With
+
+* [Cloudscraper](https://www.npmjs.com/package/cloudscraper) - Used for bypassing Cyber's detection
+* [Node-Notifier](https://www.npmjs.com/package/node-notifier) - Sending desktop notifications
+* [Opn](https://www.npmjs.com/package/opn) - Opening the Cyber page in your web browser
+
+## Authors
+
+* **ryxnSZN** - *Developer* - [Twitter](https://twitter.com/ryxnSZN)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
---
+++ monitor.js
@@ -0,0 +1,70 @@
+const cloudscraper = require('cloudscraper');
+const path = require('path')
+const opn = require('opn')
+const fs = require('fs')
+const readline = require('readline');
+const config = require('./config.json')
+const notifier = require('node-notifier');
+
+let restockLive = false
+
+let proxyArray = []
+
+let requestOptions = {
+  method: 'GET',
+  url: 'https://www.cybersole.io/api/shop/purchase'
+}
+
+let currentProxy = 0
+
+function checkCyber() {
+  if (proxyArray.length > 0) {
+    requestOptions.proxy = proxyArray[currentProxy]
+    currentProxy++;
+    if (currentProxy > proxyArray.length)
+      currentProxy = 0
+  }
+  console.log('Checking Cyber...')
+  cloudscraper.request(requestOptions, function (error, response, body) {
+    if (error) {
+      console.log(error)
+      console.log('Error occurred');
+    } else {
+      data = JSON.parse(body)
+
+      console.log('In Stock?: ' + data.available)
+      if (data.available && !restockLive) {
+        restockLive = true
+        console.log('Restocked!!')
+        notifier.notify({
+          title: 'Cybersole Restock',
+          message: `Question: ${data.question}`,
+          icon: path.join(__dirname, 'cybersole.jpg'),
+        })
+        notifier.on('click', function () {
+          opn('https://www.cybersole.io/')
+        });
+      }
+      else if (!data.available && restockLive)
+        restockLive = false
+
+      setTimeout(checkCyber, config.monitorDelay)
+    }
+  });
+}
+
+readline.createInterface({
+  input: fs.createReadStream(path.join(__dirname, config.proxyFile)),
+  terminal: false
+}).on('line', function (line) {
+  let proxy = line.split(':')
+  let proxyString
+  if (proxy.length > 2)
+    proxyString = `http://${proxy[2]}:${proxy[3]}@${proxy[0]}:${proxy[1]}`
+  else
+    proxyString = `http://${proxy[0]}:${proxy[1]}`
+
+  proxyArray.push(proxyString)
+}).on('close', function () {
+  checkCyber()
+});
