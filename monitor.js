435be939c507045554eb4b6e5d6aa71214e188dd
Ryxn <purkaitryan@gmail.com>
2018-07-06 00:15:43 -0700

    Monitor v1.1
    Code Cleanup
    Added the ability to "reply" with the answer and directly open checkout page (mac only rn)

--- monitor.js
+++ monitor.js
@@ -5,16 +5,17 @@ const fs = require('fs')
 const readline = require('readline');
 const config = require('./config.json')
 const notifier = require('node-notifier');
-var hookcord = require('hookcord');
+const hookcord = require('hookcord');
 
-var Base = hookcord.Base;
+
+let Base = hookcord.Base;
 let restockLive = false
 
 let proxyArray = []
 
 let requestOptions = {
   method: 'GET',
-  url: 'https://www.cybersole.io/api/shop/purchase'
+  url: 'https://www.cybersole.io/api/shop/purchase',
 }
 
 let currentProxy = 0
@@ -26,56 +27,79 @@ function checkCyber() {
     if (currentProxy > proxyArray.length)
       currentProxy = 0
   }
-  console.log('Checking Cyber...')
-  cloudscraper.request(requestOptions, function (error, response, body) {
-    if (error) {
-      console.log(error)
-      console.log('Error occurred');
-    } else {
-      data = JSON.parse(body)
 
-      console.log('In Stock?: ' + data.available)
-      if (data.available && !restockLive) {
-        restockLive = true
-        console.log('Restocked!!')
-        var hook = new Base("", {'link': config.discordWebhook}, {
-          'embeds': [{
-            'title': 'Cybersole Restock',
-            'url': 'https://www.cybersole.io/',
-            'color': '65280',
-            'fields': [{
-              'name': 'Question',
-              'value': `${data.question}`,
-              'inline': true
-            },
-            {
-              'name': 'Link',
-              'value': 'https://www.cybersole.io/',
-              'inline': false
-            }],
-            'thumbnail': {
-              'url': 'https://pbs.twimg.com/profile_images/982280328143392769/_KELHBk9_400x400.jpg'
-            },
-            'timestamp': new Date()
-          }]
-        });
-        hook.send().then(function(request) {
-          console.log('Sent To Discord')
-        });
-        notifier.notify({
-          title: 'Cybersole Restock',
-          message: `Question: ${data.question}`,
-          icon: path.join(__dirname, 'cybersole.jpg'),
-        })
-        notifier.on('click', function () {
-          opn('https://www.cybersole.io/')
-        });
-      }
-      else if (!data.available && restockLive)
-        restockLive = false
-        
-      setTimeout(checkCyber, config.monitorDelay)
+  console.log('Checking Cyber...')
+
+  cloudscraper.request(requestOptions, function (err, res, body) {
+
+    if (err) {
+      console.log(err)
+      console.log('Error occurred');
+      return
     }
+
+    data = JSON.parse(body)
+
+    console.log('In Stock?: ' + data.available)
+
+    if (data.available && !restockLive) {
+
+      restockLive = true
+
+      console.log('Restocked!!')
+
+      var hook = config.discordWebhook ? new Base("", { 'link': config.discordWebhook }, {
+        'embeds': [{
+          'title': 'Cybersole Restock',
+          'url': 'https://www.cybersole.io/',
+          'color': '65280',
+          'fields': [{
+            'name': 'Question',
+            'value': `${data.question}`,
+            'inline': true
+          },
+          {
+            'name': 'Link',
+            'value': 'https://www.cybersole.io/',
+            'inline': false
+          }],
+          'thumbnail': {
+            'url': 'https://pbs.twimg.com/profile_images/982280328143392769/_KELHBk9_400x400.jpg'
+          },
+          'timestamp': new Date()
+        }]
+      }) : null;
+      config.discordWebhook ? hook.send().then(function (request) {
+        console.log('Sent To Discord')
+      }) : null;
+
+      notifier.notify({
+        title: 'Cybersole Restock',
+        message: `Question: ${data.question}`,
+        icon: path.join(__dirname, 'cybersole.jpg'),
+        reply: true
+      })
+      notifier.on('replied', function (obj, options, metadata) {
+        let tempRequest = requestOptions;
+        tempRequest.url = 'https://cybersole.io/api/shop/purchase?answer=' + metadata.activationValue
+
+        cloudscraper.request(tempRequest, function (err, res, body) {
+          data = JSON.parse(body)
+
+          if(data.correct) {
+            opn(data.url)
+          }
+        })
+
+      });
+      notifier.on('click', function () {
+        opn('https://www.cybersole.io/')
+      });
+
+    } else if (!data.available && restockLive)
+      restockLive = false
+
+    setTimeout(checkCyber, config.monitorDelay)
   });
 }
 
